fe06a9e48d10c56eb4aadc27b06316f8b00eb745
nonowar7 <sabalanoam77@gmail.com>
2018-12-30 22:11:37 +0200

    adding notes to the program

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include "Runner.h"
 
-
 // program initialization
 int main (int argc, char* argv[]) {
+
     try{
         Runner runner;
         runner.run(argc, argv);
-    }catch(...) {
+    } catch(...) {
         throw "could not run program";
     }
     return 0;
